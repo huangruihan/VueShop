3d049beabd298106d045685e16e19dc768acc107
huangruihan <875136964@qq.com>
2018-07-25 19:32:30 +0800

    搭建好项目基本架构

---
+++ src/components/news.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="news">
+    <h2>我是国家大事</h2>
+    <ul>
+        <li>疫苗竟然造价</li>
+        <li>造价老板没人性</li>
+        <li>带着小姨子跑啦</li>
+        <li>我们没有办法投疫苗抵工资</li>
+    </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    h2{
+        background-color: skyblue;
+    }
+    li{
+        background-color: yellowgreen;
+    }
+</style>
+
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
+//引入vue模块
 import Vue from 'vue'
+//引入路由模块
+// import VueRouter from 'vue-router'
+//引入app组件根节点
 import App from './App.vue'
+//引入路由
+// import news from './components/news.vue';
+// import star from './components/star.vue';
 
+//引入CSS
+import './assets/statics/site/css/style.css';
+
+//路由中间件
+// Vue.use(VueRouter)
+//定义路由规则
+// const router = new VueRouter({
+//     routes: [
+//       {path:'/news',component:news},
+//       {path:'/star',component:star}
+//     ]
+// })
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  el:"#app",
+  //渲染APP组件
+  render: h => h(App),
+  // //挂载路由规则
+  // router
 }).$mount('#app')
